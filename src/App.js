3faaf28c93054f6068e0a5fafea805ce149780b4
mizanor21 <mizanorrahman398@gmail.com>
2022-10-11 01:35:01 +0600

    Navbar area, router, and footer area added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './components/Blog/Blog';
+import Statistics from './components/Statistics/Statistics';
+import Topics from './components/Topics/Topics';
 import Main from './layout/Main';
 
 function App() {
@@ -7,6 +10,20 @@ function App() {
     {
       path: '/',
       element: <Main></Main>,
+      children: ([
+        {
+          path: '/topics',
+          element: <Topics></Topics>
+        },
+        {
+          path: '/statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        }
+      ])
 
     }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Navbar.css';
+import logo from '../../assets/img/Quiz-Logo.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>this nav area!</h2>
+        <div className="bg-none">
+            <div className="container mx-auto navbar">
+                <div className="flex-1 logo">
+                    <Link to={'/'} className="btn btn-ghost btn-outline normal-case text-xl"> <img className='h-24' src={logo} alt="" /> Quiz Time</Link>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal p-0 text-xl font-bold">
+                        <li className='navItems rounded-lg'><Link to={'/topics'}>Topics</Link></li>
+                        <li className='navItems rounded-lg'><Link to={'/statistics'}>Statistics</Link></li>
+                        <li className='navItems rounded-lg'><Link to={'/blog'}>Blog</Link></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
