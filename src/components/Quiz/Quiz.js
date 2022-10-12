82fb3368e3c05c7ba12f700ed243254fcdb0a250
mizanor21 <mizanorrahman398@gmail.com>
2022-10-12 20:53:23 +0600

    Quiz part some add and eidtted

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -7,7 +7,7 @@ const Quiz = () => {
     const { id, name, questions } = quiz.data;
     return (
         <div className='m-5'>
-            <h2 className='text-xl font-bold text-center border-l-black'>Quiz of {name}</h2>
+            <h2 className='text-2xl font-bold text-center bg-gradient-to-r from-indigo-300 via-purple-400 to-pink-300 text-lg shadow-2xl p-5 w-80 rounded-tl-3xl rounded-br-3xl  mx-auto'>Quiz of <span className='text-green-600 text-3xl '>{name}</span></h2>
 
             <div className="questions">
                 {
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -11,13 +11,16 @@ const Statistics = () => {
     return (
         <div className='chart text-center'>
 
-            <LineChart className='line-chart' width={800} height={400} data={data.data} margin={{ top: 20, right: 20, bottom: 5, left: 0 }}>
-                <Line type="monotone" dataKey="total" stroke="#8884d8" />
-                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                <XAxis dataKey="name" />
-                <YAxis dataKey='total' />
-                <Tooltip />
-            </LineChart>
+            <div className="rechart">
+                <LineChart className='line-chart' width={800} height={400} data={data.data} margin={{ top: 20, right: 20, bottom: 5, left: 0 }}>
+                    <Line type="monotone" dataKey="total" stroke="#8884d8" />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <XAxis dataKey="name" />
+                    <YAxis dataKey='total' />
+                    <Tooltip />
+                </LineChart>
+                <h2 className='text-xl fill-emerald-400 font-bold py-5'>Re-Chart</h2>
+            </div>
 
         </div>
     );
